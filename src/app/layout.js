0a81a2ba283fa2de381636ddea0eb8d979e5be6f
Yuki-Hub98 <wellyson.yago1@gmail.com>
2023-11-07 22:16:36 -0400

    Adicionando nav no layout para todas as paginas herdarem

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,5 @@
 import { Inter } from 'next/font/google'
-import './globals.css'
+import "../styles/globals.css"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,7 +11,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <nav className='w-full h-14 bg-sky-600'> </nav>
+          {children}
+        
+      </body>
     </html>
   )
 }
