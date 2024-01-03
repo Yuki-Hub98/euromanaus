a942f1a5769be3c2d4755473effe69c2a0723f80
Yuki-Hub98 <wellyson.yago1@gmail.com>
2024-01-03 18:03:49 -0400

    Tentando setar minha paleta no tailwindo css

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,13 +9,11 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors:{
+        white:"#FFFFFF"
       },
     },
   },
   darkMode: "class",
-  plugins: [nextui()],
+  plugins: [nextui({ addCommonColors: true})],
 }
