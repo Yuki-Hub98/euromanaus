450a417d66e34a272a244014ae428d1ccf3a23c0
Yuki-Hub98 <wellyson.yago1@gmail.com>
2024-01-04 16:13:10 -0400

    Alterando a tela de cadastro de fornecedor, para ficar so o componente

--- src/app/cadastro-fornecedor/page.js
+++ src/app/cadastro-fornecedor/page.js
@@ -1,98 +1,25 @@
 "use client";
 
-import React, { useState } from "react";
-import { Modal, Button, useDisclosure, ModalContent, ModalHeader, ModalBody, ModalFooter,Input,Tabs, Tab, Link, Card, CardBody, CardHeader, } from "@nextui-org/react";
+import React from "react";
+import Cadastro from '@/components/Cadastro';
 
-export default function cadastroFun () {
-  const {isOpen, onOpen, onOpenChange} = useDisclosure();
-  const [selected, setSelected] = useState("Social");
+export default function CadFornecedor () {
+  
+
     return (
-        <div className="mt-2.5">
-          <Button onPress={onOpen} color="primary">
-            Cadastro de Forncedor
-          </Button>
-          <Modal
-          isOpen={isOpen}
-          onOpenChange={onOpenChange}
-          placement="top-center"
-          size="5xl"
-          >
-            <ModalContent >
-            {(onClose) => (
-                  <>
-                    <ModalHeader className="flex flex-col gap-1 text-center"> Cadastro de Fornecedor </ModalHeader>
-                    <ModalBody>
-                      <div className="flex flex-col w-full">
-                        <Card className="max-w-full justify-items-center h-[520px]">
-                          <CardBody className="overflow-hidden">
-                            <Tabs
-                              fullWidth
-                              size="md"
-                              aria-label="Tabs form"
-                              selectedKey={selected}
-                              onSelectionChange={setSelected}>
-                              <Tab key={"social"} title={"Social"}>
-                                <form>
-                                  <div className="w-full flex flex-row gap-2 justify-items-stretch">
-                                    <div>
-                                      <Input label="Nome Fantasia" type="Text" labelPlacement="outside-left" className="mt-2 justify-between" color="white" size="" />
-                                      <Input label="Razão Social" type="Text" labelPlacement="outside-left" className="mt-2 justify-between"  />
-                                      <Input label="CPF/CNPJ" type="Text" labelPlacement="outside-left" className="mt-2 justify-between" />
-                                      <Input label="INSCR. Social/ RG" type="Text" labelPlacement="outside-left" className="mt-2 justify-between"  />
-                                    </div>
-                                    <div> 
-                                      <Input label="CEP" type="Text" labelPlacement="outside-left" className="mt-2 justify-between"  />
-                                      <Input label="Endereço" type="Text" labelPlacement="outside-left" className="mt-2 justify-between"   />
-                                      <Input label="Número" type="Text" labelPlacement="outside-left" className="mt-2 justify-between"  />
-                                      <Input label="Bairro" type="Text" labelPlacement="outside-left" className="mt-2 justify-between"  />
-                                      <Input label="Cidade" type="Text" labelPlacement="outside-left" className="mt-2 justify-between"  />
-                                      <Input label="UF" type="Text" labelPlacement="outside-left" className="mt-2 justify-between"  />
-                                    </div>
-                                  </div>
-                                </form>
-                              </Tab>
-                              <Tab key={"contato"} title={"Contato"}>
-                                <form>
-                                  <div className="w-full flex flex-row gap-2 justify-items-stretch">
-                                    <div>
-                                      <Input label="Telefone" type="Text" labelPlacement="outside-left" className="mt-2 justify-between" />
-                                      <Input label="Celular" type="Text" labelPlacement="outside-left" className="mt-2 justify-between" />
-                                      <Input label="E-mail" type="Text" labelPlacement="outside-left" className="mt-2 justify-between" />
-                                      <Input label="Contato" type="Text" labelPlacement="outside-left" className="mt-2 justify-between" />
-                                    </div>
-                                  </div>
-                                </form>
-                              </Tab>
-                              <Tab key={"Outros"} title={"Outros"}>
-                                <form>
-                                  <div className="w-full flex flex-row gap-2 justify-items-stretch">
-                                    <div>
-                                      <Input label="Dados Bancarios" type="Text" labelPlacement="outside-left" className="mt-2 justify-between"  />
-                                      <Input label="Tarara" type="Text" labelPlacement="outside-left" className="mt-2 justify-between"  />
-                                      <Input label="Tarara" type="Text" labelPlacement="outside-left" className="mt-2 justify-between"  />
-                                      <Input label="Tarara" type="Text" labelPlacement="outside-left" className="mt-2 justify-between"  />
-                                    </div>
-                                  </div>
-                                </form>
-                              </Tab>
-                            </Tabs>
-                          </CardBody>
-                        </Card>
-                      </div>
-                    </ModalBody>
-                    <ModalFooter>
-                      <Button color="danger" variant="flat" onPress={onClose}>
-                        {console.log(onClose)}
-                        Cancelar
-                      </Button>
-                      <Button color="primary" onPress={onClose}>
-                        Cadastrar
-                      </Button>
-                    </ModalFooter>
-                  </>
-              )}
-            </ModalContent>
-          </Modal>
-        </div>
+      <>
+      <nav className="flex justify-between items-center px-4 py-2">
+      <div className="flex-grow">
+          <input
+          type="text"
+          className="w-3/4 bg-gray-700 text-gray-200 px-4 py-2 rounded-md"
+          placeholder="Search..."
+          />
+      </div>
+      <div className="flex items-center"></div>
+        <Cadastro/>
+      </nav>
+      </>
+      
     )
 }
