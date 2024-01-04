5aa29978bc73cf92bc2d4c7b34dfc46d67add39d
Yuki-Hub98 <wellyson.yago1@gmail.com>
2024-01-04 16:07:30 -0400

    Tela de movimento

---
+++ src/app/movimento/page.js
@@ -0,0 +1,24 @@
+"use client";
+
+import React from "react";
+
+export default function Movimento () {
+
+
+    return (
+        <>
+        <nav className="flex justify-between items-center px-4 py-2">
+        <div className="flex-grow">
+            <input
+            type="text"
+            className="w-3/4 bg-gray-700 text-gray-200 px-4 py-2 rounded-md"
+            placeholder="Search..."
+            />
+        </div>
+        <div className="flex items-center">
+            <h1>Movimento</h1>
+        </div>
+        </nav>
+        </>
+    )
+}
