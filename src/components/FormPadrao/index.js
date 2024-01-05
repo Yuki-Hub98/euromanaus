026375b8bd604fc9ffebebc2d8df04f9b12e3dfa
Yuki-Hub98 <wellyson.yago1@gmail.com>
2024-01-05 14:19:15 -0400

    Criação form padrão

---
+++ src/components/FormPadrao/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { Input } from "@nextui-org/react"
+const FormPadrao = () =>{
+    return(
+            <Input
+                type="email"
+                label="Email"
+                labelPlacement="outside-left"
+            />
+        
+    )
+}
+
+export default FormPadrao
