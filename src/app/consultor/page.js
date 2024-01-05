121984d33e977fc983af359c169d766d08e3caf4
Yuki-Hub98 <wellyson.yago1@gmail.com>
2024-01-05 14:17:30 -0400

    Adicionando as paginas novas de acordo com o dropdown

---
+++ src/app/consultor/page.js
@@ -0,0 +1,26 @@
+"use client";
+
+import React from "react";
+import Cadastro from '@/components/Cadastro';
+
+export default function Consultor () {
+
+
+    return (
+        <>
+        <nav className="flex justify-between items-center px-4 py-2">
+        <div className="flex-grow">
+            <input
+            type="text"
+            className="w-3/4 bg-gray-700 text-gray-200 px-4 py-2 rounded-md"
+            placeholder="Search..."
+            />
+        </div>
+        <div className="flex items-center">
+            <h1>Consultor</h1>
+        </div>
+            <Cadastro/>
+        </nav>
+        </>
+    )
+}
---
+++ src/app/tipo-de-notas/page.js
@@ -0,0 +1,23 @@
+"use client";
+
+import React from "react";
+import Cadastro from '@/components/Cadastro';
+
+export default function TiposNotas () {
+
+    return (
+        <>
+        <nav className="flex justify-between items-center px-4 py-2">
+        <div className="flex-grow">
+            <input
+            type="text"
+            className="w-3/4 bg-gray-700 text-gray-200 px-4 py-2 rounded-md"
+            placeholder="Search..."
+            />
+        </div>
+        <div className="flex items-center"></div>
+            <Cadastro/>
+        </nav>
+        </>
+    )
+}
