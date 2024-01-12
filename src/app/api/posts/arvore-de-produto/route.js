a5acd75bf31cd90d04c70cf583c6ea9e5a8b656a
Yuki-Hub98 <wellyson.yago1@gmail.com>
2024-01-12 17:58:13 -0400

    Mais um teste

--- src/app/api/posts/arvore-de-produto/route.js
+++ src/app/api/posts/arvore-de-produto/route.js
@@ -1,18 +1,18 @@
 
-export async function POST(data){
+export async function POST(request){
 
-    console.log("Data: ", data)
+    const res = await request.json()
 
     const response = await fetch("http://localhost:8080/arvore-produto/departamento", {
         method:"POST",
         headers:{
             'content-type': 'application/json'
         },
-        body: JSON.stringify(data)
+        body: JSON.stringify(res)
     }).then((response) => response.json())
     
     if (!response.ok) throw new Error("Algum problema");
 
-    return response
+    console.log(response) 
 
 }
