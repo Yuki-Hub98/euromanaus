7ea68f4525152ec9330778982beb3de4796ed66c
Yuki-Hub98 <wellyson.yago1@gmail.com>
2024-01-16 14:18:42 -0400

    Tentiva de criar um tema padrão no Next Ui, deu certo mas não é definitivo

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,7 +18,46 @@ module.exports = {
     },
   },
   darkMode: "class",
-  plugins: [nextui({ addCommonColors: true})],
+  plugins: [nextui({
+    addCommonColors: false,
+    themes: {
+      "purple-dark": {
+        extend: "dark", // <- inherit default values from dark theme
+        colors: {
+          background: "#1a1600",
+          foreground: "#ffffff",
+          primary: {
+            50: "#6c5c09",
+            100: "#83700f",
+            200: "#a28d18",
+            300: "#c2af23",
+            400: "#e2c731",
+            500: "#edd162",
+            600: "#f6e382",
+            700: "#fcf0ad",
+            800: "#fdf8d5",
+            900: "#fefeec",
+            DEFAULT: "#edca62",
+            foreground: "#ffffff",
+          },
+          focus: "#F182F6",
+        },
+        layout: {
+          disabledOpacity: "0.3",
+          radius: {
+            small: "4px",
+            medium: "6px",
+            large: "8px",
+          },
+          borderWidth: {
+            small: "1px",
+            medium: "2px",
+            large: "3px",
+          },
+        },
+      },
+    },
+  }, { })],
 }, withMT({
   content: [],
   theme: {
