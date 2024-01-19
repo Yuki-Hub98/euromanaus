75145868a94379eac29832cfc9141784b20cf456
Yuki-Hub98 <wellyson.yago1@gmail.com>
2024-01-19 15:49:16 -0400

    Att: Adicionei uma verificação, para não mandarem string vazia nem com espaço

--- src/functions/regexToSave.js
+++ src/functions/regexToSave.js
@@ -2,6 +2,13 @@ const RegexToSave = (param) => {
 
     let string = ''
     string = param.toUpperCase()
+    for (let i = 0; i < string.length; i++) {
+        if (param[0] === " ") {
+            return string = ''
+        }else if(param[i] === "  "){
+            return string = ''
+        }
+    }
     return string
 }
 
