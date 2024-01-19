77559a3282d10da4e6b16b063f4fed5aa1d975c1
Yuki-Hub98 <wellyson.yago1@gmail.com>
2024-01-19 15:52:59 -0400

    Att: Esse post estava com problema de promisses e requisições de funções, então refiz para responder a um promisse e retornar sempre os valores nas requisições

--- src/pages/api/arvore-produto/post.js
+++ src/pages/api/arvore-produto/post.js
@@ -1,11 +1,30 @@
 import axios from "axios"
 
 const Post = async (data, request) =>{
-    const response = axios.post(`http://localhost:8080/arvore-produto/${request}`,data)
-    .then((response) => console.log("Resposta: ", response))
-    .catch((error) => console.log("Error: ", error))
+    
+    const response = await axios.post(`http://localhost:8080/arvore-produto/${request}`, data)
+    .then((response) => {
+        return response.data
+    })
+    .catch((error) => {
+        if (error?.response) {
+            // A requisição foi feita e o servidor respondeu com um código de status
+            // que sai do alcance de 2xx
+            return error.response.data
+        } else if (error.request) {
+            // A requisição foi feita mas nenhuma resposta foi recebida
+            // `error.request` é uma instância do XMLHttpRequest no navegador e uma instância de
+            // http.ClientRequest no node.js
+            console.error(error.request);
+        } else {
+            // Alguma coisa acontenceu ao configurar a requisição que acionou este erro.
+            console.error('Error', error.message);
+        }
+            console.error(error.config);
+    });
 
-
+    return response
+    
 }
 
 
