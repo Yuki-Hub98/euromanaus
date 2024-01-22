967e902c51c80b0da7e6503d3100d59dc4fa9b54
Yuki-Hub98 <wellyson.yago1@gmail.com>
2024-01-22 17:49:42 -0400

    Att: Principais alterações foram: o modal virou um component fora do principal, a condição do warning foi melhorada, a pesquisa virou um component separado, agora contem um component de alerta de cadastro, troquei o nome de algumas variaveis para fazer sentido

--- src/components/CadastroArvore/cadastroArvore.js
+++ src/components/CadastroArvore/cadastroArvore.js
@@ -1,17 +1,62 @@
+"use client";
+import React , {useEffect, useState} from "react";
 import { Modal, Button, ModalContent, ModalHeader, ModalBody, 
-    ModalFooter, Input} from "@nextui-org/react";
+    ModalFooter, Input, useDisclosure} from "@nextui-org/react";
 import Select from "react-select";
+import RegexToSave from "@/functions/regexToSave";
 
 const CadastroModal = (data) => {
 
+    const [dataToPost, setDataToPost] = useState();
+    const [selectData, setSelectData] = useState();
+    const [dataDescricao, setDataDescricao] = useState();
+    
+
+    const dataTransform = data?.dataModal?.map((data) => ( 
+        {'value': data?.descricao, 'label':data?.descricao}
+        ))
+    
+    const FormateToPost = (opcao) =>{
+        switch (opcao) {
+            case 'Departamento':
+                setDataToPost({'descricao': dataDescricao})
+                break;
+            case 'Linha':
+                setDataToPost({'departamento': selectData, 'descricao': dataDescricao})
+                break;
+            case 'Familia':
+                setDataToPost({'linha': selectData, 'descricao': dataDescricao})
+                break;
+            case 'Grupo':
+                setDataToPost({'familia': selectData, 'descricao': dataDescricao})
+                break;
+            case 'Cor':
+                setDataToPost({'descricao': dataDescricao})
+                break;
+            case 'Especificação':
+                setDataToPost({'descricao': dataDescricao})
+                break;
+            default:
+                break;
+        }
+        
+    }
+
+    useEffect(() => {
+        if (dataToPost) {
+            setDataDescricao(null)
+            return data?.ReceivePostData(dataToPost)  
+        }
+    }, [dataToPost])
+    
     return (
         <>
         <Modal 
-            isOpen={isOpen}
-            onOpenChange={onOpenChange}
+            isOpen={data?.isOpen}
+            onOpenChange={data?.onOpenChange}
             placement="top-center"
             size="md"
-            className=' h-80'
+            className='h-3/6'
             classNames={{
             body: "py-6",
             backdrop: "bg-[#D4D4D8]/50 backdrop-opacity-40",
@@ -24,31 +69,31 @@ const CadastroModal = (data) => {
             <ModalContent>
                 {(onClose) => (
                     <>
-                    <ModalHeader className="flex flex-col gap-1"> {value.name} </ModalHeader>
+                    <ModalHeader className="flex flex-col gap-1"> {data?.data?.name} </ModalHeader>
                     <ModalBody>
                     <div className="flex flex-col w-full">
                         <div className="w-full flex flex-row gap-2">
-                            {value.type === 1 ? 
+                            {data?.data?.type === 1 ? 
                                 <div>
                                 <Input label="Descrição"  size='lg' type="Text" onChange={(e) => setDataDescricao(RegexToSave(e.target.value))} labelPlacement="outside-left" className="mt-2 w-80 justify-between"/>
                                 </div>
                                 :
                                 <div>
-                                <div className='flex justify-center w-96 relative'>
+                                <div className='flex justify-center  w-96 relative'>
                                     <Select className='w-60 ml-3' onChange={(e) => setSelectData(e.value)} options={dataTransform}/>
-                                        </div>
-                                            <Input label="Descrição" size='lg' type="Text" onChange={(e) => setDataDescricao(RegexToSave(e.target.value))} labelPlacement="outside-left" className="mt-2 w-80 justify-between"/>
-                                        </div>
+                                </div>
+                                <Input label="Descrição" size='lg' type="Text" onChange={(e) => setDataDescricao(RegexToSave(e.target.value))} labelPlacement="outside-left" className="mt-2 w-80 justify-between"/>
+                                </div>
                             }
                         </div>
                     </div>
                     </ModalBody>
                     <ModalFooter>
-                        <Button className='bg-sky-50' variant="flat" onClick={() => {setData(null), CloseStatus()}} onPress={onClose}>
+                        <Button className='bg-sky-50' variant="flat" onClick={() => {setDataToPost(null), data?.CloseStatus()}} onPress={onClose} >
                             Cancelar
                         </Button>
                             { dataDescricao ? (
-                                <Button className="bg-[#edca62b4] shadow-lg shadow-indigo-500/20" onClick={() => FormateToPost(value.name)} onPress={onClose}>
+                                <Button className="bg-[#edca62b4] shadow-lg shadow-indigo-500/20" onClick={() => {FormateToPost(data?.data?.name)}} onPress={onClose} >
                                     Cadastrar
                                 </Button>
                             )
