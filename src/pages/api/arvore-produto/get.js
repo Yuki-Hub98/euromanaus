1970b21aa8d2051d82b99090844c081360f1cbda
Yuki-Hub98 <wellyson.yago1@gmail.com>
2024-01-23 17:51:51 -0400

    Att: Nova funcionalidade de busca implementada, agora um get sera feito com parametro se estiver com parametro

--- src/pages/api/arvore-produto/get.js
+++ src/pages/api/arvore-produto/get.js
@@ -1,13 +1,17 @@
 import axios from "axios";
 
-const Get = async (request) =>{
-    const response = await axios.get(`http://localhost:8080/arvore-produto/${request}`)
-    .then((response) => response.data )
-    .catch((error) => error.response)
-
-    
-    return response
-    
+const Get = async (request, data) =>{
+    if (data){
+        const response = await axios.get(`http://localhost:8080/arvore-produto/${request}/search`,{params: data})
+        .then((response) => response.data )
+        .catch((error) => error.response)
+        return response
+    }else{
+        const response = await axios.get(`http://localhost:8080/arvore-produto/${request}`)
+        .then((response) => response.data )
+        .catch((error) => error.response)
+        return response
+    }
 }
 
 export default Get
