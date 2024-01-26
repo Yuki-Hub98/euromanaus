9ac8997dbfebf40329b508f998188b3224a7849b
Yuki-Hub98 <wellyson.yago1@gmail.com>
2024-01-26 16:20:02 -0400

    Att: Pequenas mudanças no Alert para o build funcionar, algumas variaveis desnecessarias foram retiradas

--- src/components/SuccessAlert/index.js
+++ src/components/SuccessAlert/index.js
@@ -1,32 +1,32 @@
 import { useState, useEffect } from 'react';
 
-const SuccessAlert = (data) => {
+const SuccessAlert = (props) => {
     const [showAlert, setShowAlert] = useState(false);
 
     useEffect(() => {
-        if (data) {
+        if (props) {
             setShowAlert(true)
         }
-    },[data])
+    },[props])
 
     useEffect(() => {
       // Mostrar o alerta quando showAlert se torna true
         if (showAlert) {
         const timeoutId = setTimeout(() => {
           // Ocultar o alerta após 3 segundos (ajuste conforme necessário)
-            data?.CloseStatus()
+            props?.CloseStatus()
             setShowAlert(false);
         }, 4000);
 
         // Limpar o temporizador ao desmontar o componente
         return () => clearTimeout(timeoutId);
         }
-    }, [showAlert]);
+    });
 
     return (
         showAlert && (
             <div className="fixed top-0 right-0 mt-4 mr-4 bg-[#2c2c2b] text-[#c2af23] p-4 rounded shadow-lg transform opacity-100 translate-y-0 transition-opacity duration-300 ease-in-out">
-                {data?.message} <strong>Sucesso!</strong> 
+                {props?.message} <strong>Sucesso!</strong> 
             </div>
         )
     );
