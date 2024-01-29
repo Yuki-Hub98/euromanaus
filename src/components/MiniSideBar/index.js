4c4941d6b7e2bfa45671194db4630c6ef612918c
Yuki-Hub98 <wellyson.yago1@gmail.com>
2024-01-29 13:25:37 -0400

    Att: Atualizando layout da MiniSideBar

--- src/components/MiniSideBar/index.js
+++ src/components/MiniSideBar/index.js
@@ -20,7 +20,7 @@ const MiniSideBar = (props) => {
 
     return(
         <>
-        <aside className='flex flex-col h-full w-40 border rounded-md bg-[#0000008e] left-64 '>
+        <aside className='flex flex-col h-full w-40 bg-[#1E1E1F] rounded left-64 '>
             {props.name?.map((name) => (
                 <button key={name} onClick={(e) => {setOption(e.target.innerText)}}  className={`flex h-9 w-64 p-2 cursor-pointer text-center  font-normal text-[#D4D4D8]  hover: decoration-solid  dark:text-gray-900 hover:border-0 hover:text-[#edca62] rounded-lg `}>
                     <span>{name}</span>
