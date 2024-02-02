873bc38299486606ddb21045c67aa15eb3656c11
Yuki-Hub98 <wellyson.yago1@gmail.com>
2024-02-02 10:36:27 -0400

    Add: Novo method adicionado é o put para arvore de produto

---
+++ src/pages/api/arvore-produto/put.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+const Put = async (request, data) =>{
+
+    const response = await axios.put(`http://localhost:8080/arvore-produto/${request}/edit`, data)
+    .then((response) => {
+        if (response) {
+            return response.data
+        }}
+    ).catch((error) => {
+        if (error?.response) {
+            // A requisição foi feita e o servidor respondeu com um código de status
+            // que sai do alcance de 2xx
+            return error.response.data
+        } else if (error.request) {
+            // A requisição foi feita mas nenhuma resposta foi recebida
+            // `error.request` é uma instância do XMLHttpRequest no navegador e uma instância de
+            // http.ClientRequest no node.js
+            console.error(error.request);
+        } else {
+            // Alguma coisa acontenceu ao configurar a requisição que acionou este erro.
+            console.error('Error', error.message);
+        }
+            console.error(error.config);
+    });
+
+    return response
+}
+
+
+export default Put
