ccb8597f253cfc0a6eea3fa97500f70b2b50698a
Yuki-Hub98 <wellyson.yago1@gmail.com>
2024-02-02 10:47:51 -0400

    Att: Foi adicionado o botão de editar e as funções e as variaveis para fazer o put no componente

--- src/components/TopButtons/index.js
+++ src/components/TopButtons/index.js
@@ -3,6 +3,7 @@ import RegisterModal from "../RegisterModal";
 import SearchArvore from "../Search";
 import {Button, useDisclosure} from "@nextui-org/react";
 import { usePathname } from "next/navigation";
+import EditModal from "../EditModal";
 
 
 const TopButtons = (props) => {
@@ -10,9 +11,16 @@ const TopButtons = (props) => {
     const { isOpen , onOpen , onOpenChange } = useDisclosure();
     const [receivePostData, setReceivePostData] = useState();
     const [receiveGetData, setReceiveGetData] = useState();
+    const [receivePutData, setReceivePutData] = useState();
     const [nameRequest, setNameRequest] = useState();
     const [nameRequestGet, setNameRequestGet] = useState();
-    
+    const [nameRequestPut, setNameRequestPut] = useState();
+    const [open, setOpen] = useState(false)
+
+    const modal = (value) =>{
+        return setOpen(value)
+    }
+
     const ReceivePost = ( nameRequest ,data) => {
         setNameRequest(nameRequest)
         setReceivePostData(data)
@@ -23,9 +31,17 @@ const TopButtons = (props) => {
         setReceiveGetData(data)
     }
 
+    const ReceivePut = (nameRequest, data) =>{
+        setNameRequestPut(nameRequest)
+        setReceivePutData(data)
+    }
+
+    
     const toClean = () => {
         setReceivePostData(null)
         setReceiveGetData(null)
+        setReceivePutData(null)
+        setNameRequestPut(null)
         setNameRequestGet(null)
         setNameRequest(null)
         
@@ -36,6 +52,16 @@ const TopButtons = (props) => {
             props?.PostData(nameRequest, receivePostData)
             return toClean()
         }
+    },[props, nameRequest, receivePostData])
+
+    useEffect(() => {
+        if (receivePutData) {
+            props?.PutData(nameRequestPut, receivePutData)
+            return toClean();
+        }
+    }, [props, nameRequestPut, receivePutData])
+
+    useEffect(() => {
         if (receiveGetData && nameRequestGet) {
             props?.GetData(nameRequestGet, receiveGetData);
             return toClean();
@@ -43,8 +69,7 @@ const TopButtons = (props) => {
             props?.GetData(nameRequestGet);
             return toClean();
         }
-        
-    },[props, nameRequest ,receiveGetData ,receivePostData , nameRequestGet])
+    },[props,receiveGetData, nameRequestGet])
 
     const OptionPage = (page) => {
         switch (page) {
@@ -60,6 +85,19 @@ const TopButtons = (props) => {
                         </Button>
                         <RegisterModal name={props?.option} isOpen={isOpen} dataModal={props?.dataModal} onOpenChange={onOpenChange} ReceivePost={ReceivePost} />
                     </div>
+                    <div className='flex items-center'>
+                        {props?.valueTable ? 
+                            <Button color="primary" size='sm' variant="ghost" onPress={() => {setOpen(true)}}>
+                                Editar
+                            </Button>
+                            :
+                            <Button color="primary" size="sm" variant="ghost" isDisabled>
+                                Editar
+                            </Button>
+                        }
+                        <EditModal name={props?.option} valueTable={props?.valueTable} ReceivePut={ReceivePut} isOpen={open} modal={modal}
+                        tableData={props?.tableData} onOpenChange={onOpenChange} />
+                    </div> 
                     </>
                 )
         
@@ -68,13 +106,10 @@ const TopButtons = (props) => {
         }
     }
 
-//#2c2c2b #0000008e
-
-
     return(
         <div className='flex flex-row pl-2 h-1/4 shadow-2xl mt-8 rounded w-full gap-2 bg-[#1E1E1F]'>
             <div className='absolute pl-2 w-full'>
-                <h1 className='font-bold text-[#D4D4D8]'>{props?.title}</h1>
+                <h1 className='font-bold text-[#D4D4D8] capitalize'>{props?.title}</h1>
             </div>
             {OptionPage(router)}
         </div>
