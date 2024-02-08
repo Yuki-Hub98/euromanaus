6c5fe9fb29c9aac6226a5a42c67e3bee423ed624
Yuki-Hub98 <wellyson.yago1@gmail.com>
2024-02-08 15:10:41 -0400

    Att: O delete estava com problemas na arvore de produtos, era porque o parametro esta um objeto dentro de outro, e o parameto do axios é só um objeto comum

--- src/pages/api/arvore-produto/del.js
+++ src/pages/api/arvore-produto/del.js
@@ -1,14 +1,13 @@
 import axios from "axios";
 const Del = async (request, data) =>{
     if (request === "linha" || request === "familia" || request === "grupo") {
-        const response = await axios.delete(`http://localhost:8080/arvore-produto/${request}/del?descricao=${data.descricao}`, {data: {data}})
+        const response = await axios.delete(`http://localhost:8080/arvore-produto/${request}/del?descricao=${data?.descricao}`, {data: data})
         .then((response) => {
             if(response){
                 return response.data
             }
         })
         .catch((error) => {
-            console.log("error: ", error)
             if (error?.response) {
                 // A requisição foi feita e o servidor respondeu com um código de status
                 // que sai do alcance de 2xx
@@ -24,7 +23,6 @@ const Del = async (request, data) =>{
             }
         })
         .catch((error) => {
-            console.log("error: ", error)
             if (error?.response) {
                 // A requisição foi feita e o servidor respondeu com um código de status
                 // que sai do alcance de 2xx
