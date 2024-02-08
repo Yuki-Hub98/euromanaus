c7280fbe46383b74a9357efc2c4edec5278a74d7
Yuki-Hub98 <wellyson.yago1@gmail.com>
2024-02-08 15:12:22 -0400

    Att: A pagina fornecedor foi iniciada vai ter o mesmo layout da arvore de produto, com alguma mudanças

--- src/app/fornecedor/page.js
+++ src/app/fornecedor/page.js
@@ -2,14 +2,24 @@
 import { Breadcrumbs, BreadcrumbItem } from "@nextui-org/react";
 import SuccessAlert from "../../components/SuccessAlert";
 import TopButtons from "@/components/TopButtons";
-import React, {useState} from "react";
-
+import MiniSideBarButtons from "@/components/MiniSideBarButtons";
+import Table from "@/components/Table";
+import React, {useEffect, useState} from "react";
 
 export default function Fornecedor () {
-    const [option, setOption] = useState('departamento');
+    const [option, setOption] = useState('fornecedor');
     const [tableData, setTableData] = useState();
     const [status, setStatus] = useState();
+    const [valueTable, setValueTable] = useState();
+    
 
+    const ValueTable = (value) => {
+        setValueTable(value)
+    }
+
+    const CloseStatus = () => {
+        return setStatus(null);
+    }
 
     return (
         <>
@@ -23,7 +33,20 @@ export default function Fornecedor () {
             </div>
                 { status?.descricao ? ( <> <SuccessAlert CloseStatus={CloseStatus} message="Cadastro efetuado com"/> </> ): (null) }
                 { status?.del ? ( <> <SuccessAlert CloseStatus={CloseStatus} message="Deletado com"/> </> ): (null) }
-            <TopButtons />
+            <TopButtons title={option} />
+            <div className='flex h-4/5 overflow-y-auto mt-1.5 w-full flex-row'>
+                <MiniSideBarButtons />
+                <div className='w-full flex overflow-y-auto h-50 flex-col rounded'>
+                    <div className='w-full top-0 overflow-y-auto h-full bg-[#EDEDED]'>
+                        <Table data={tableData} name={option} ValueTable={ValueTable} />
+                    </div>
+                    <div className='w-full h-10 bg-[#CFCFCF]'>
+                        <span className='text-black'>
+                            teste teste teste teste
+                        </span>
+                    </div>
+                </div>
+            </div>
             
         </div>
         </>
