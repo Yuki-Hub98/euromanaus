c5d4dadba1a945d9c1ecf2681d5d0e215f604569
Yuki-Hub98 <wellyson.yago1@gmail.com>
2024-02-09 14:37:38 -0400

    Att: Agora a requisição de pesquisar cep, aceita parametro caso a pessoa não saiba o cep, parametro: uf, cidade e endereco

--- src/pages/api/fornecedor/cep.js
+++ src/pages/api/fornecedor/cep.js
@@ -1,7 +1,23 @@
 import axios from "axios";
 
 const Cep = async (data) => {
-    console.log(data)
+
+    if (data?.endereco && data?.cidade && data?.uf) {
+        const response = await axios.get(`https://viacep.com.br/ws/${data?.uf}/${data?.cidade}/${data?.endereco}/json/`)
+        .then((response) => {
+            if (response) {
+                return response.data
+                }
+        })
+        .catch((error) => {
+            if (error) {
+                console.log("error: ", error)
+            }
+        })
+
+        return response
+    }
+
     const response = await axios.get(`https://viacep.com.br/ws/${data}/json/`)
     .then((response) => {
         if (response) {
@@ -14,7 +30,7 @@ const Cep = async (data) => {
         }
     })
 
-    return response
+    return response 
 }
 
 export default Cep
