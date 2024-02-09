69a40c4406870429cf5620e21776cad571359e11
Yuki-Hub98 <wellyson.yago1@gmail.com>
2024-02-09 14:45:00 -0400

    Att: Adicionando propriedade nova para o chrome não fica dando dica

--- src/components/FormRegister/registerPessoa.js
+++ src/components/FormRegister/registerPessoa.js
@@ -15,34 +15,34 @@ const RegisterPessoa = (props) => {
                 <div className="w-full p-2">
                     <div className="w-full flex flex-row gap-3 pl-1 mb-2 items-center">
                         <label form="razaoSocial" className=' text-xs' >Razão Social</label>
-                        <Input className="w-96 pl-1" size="sm" value={dataFornecedor?.razaoSocialFornecedor || ''} labelPlacement="outside" name={`razaoSocial${type}`}
-                        onChange={(e) => {handleChange(e)}}/>
+                        <Input className="w-96 pl-1" size="sm" value={dataFornecedor?.razaoSocialFornecedor || ''} labelPlacement="outside" name={`razaoSocial${type}`} 
+                        autoComplete="off" onChange={(e) => {handleChange(e)}}/>
                     </div>
                     <div className="w-full flex flex-row gap-9 pl-1 mb-2 items-center">
                         <label form="nomeFantasia" className=' text-xs' >Fantasia</label>
                             <Input className="w-96 pl-1" size="sm" value={dataFornecedor?.nomeFantasiaFornecedor || ''} labelPlacement="outside" name={`nomeFantasia${type}`}
-                            placeholder=" " onChange={(e) => {handleChange(e)}}/>
+                            autoComplete="off" placeholder=" " onChange={(e) => {handleChange(e)}}/>
                     </div>
                     <div className="w-full flex flex-row gap-3 mb-2 items-center">
                         <div className="w-[60%] flex flex-row items-center pl-1 gap-7">
                             <label className='text-xs'>CPF/CNPJ</label>
                             <Input className="w-96 pl-0.5" size="sm" value={dataFornecedor?.cpfCnpjFornecedor || ''} labelPlacement="outside" name={`cpfCnpj${type}`}
-                            onChange={(e) => {handleChange(e)}}/>
+                            autoComplete="off" onChange={(e) => {handleChange(e)}}/>
                         </div>
                         <label className='text-xs pl-6'>IE/RG</label>
                         <Input className="w-[9rem]" size="sm" value={dataFornecedor?.ieRgFornecedor || ''} labelPlacement="outside" name={`ieRg${type}`}
-                        onChange={(e) => {handleChange(e)}}/>
+                        autoComplete="off" onChange={(e) => {handleChange(e)}}/>
                     </div>
                     <div className="w-full flex flex-row gap-2 pl-0.5 mb-2 items-center">
                         <label className='text-xs' >Orgão Emissor</label>
                         <Input className="w-30" size="sm" value={dataFornecedor?.orgaoEmissorFornecedor || ''} labelPlacement="outside" name={`orgaoEmissor${type}`}
-                        onChange={(e) => {handleChange(e)}}/>
+                        autoComplete="off" onChange={(e) => {handleChange(e)}}/>
                         <label className='text-xs' >UF</label>
                         <Input className="w-28" size="sm" value={dataFornecedor?.ufRgFornecedor || ''} labelPlacement="outside" name={`ufRg${type}`}
-                        onChange={(e) => {handleChange(e)}}/>
+                        autoComplete="off" onChange={(e) => {handleChange(e)}}/>
                         <label className='text-xs' >Data de Emissão</label>
                         <Input className="w-30" type="date" value={dataFornecedor?.dataEmissaoFornecedor || ''} size="sm" labelPlacement="outside" name={`dataEmissao${type}`}
-                        onChange={(e) => {handleChange(e)}}/>
+                        autoComplete="off" onChange={(e) => {handleChange(e)}}/>
                     </div>
                 </div>
                 </>
@@ -55,33 +55,33 @@ const RegisterPessoa = (props) => {
                         <div className="w-full flex flex-row  gap-3 pl-1 mb-2 items-center">
                             <label form="razaoSocial" className=' text-xs' >Razão Social</label>
                             <Input className="w-96 pl-1" size="sm" value={dataRepresentante?.razaoSocialRepresentante || ''}  labelPlacement="outside" name={`razaoSocial${type}`}
-                            onChange={(e) => {handleChange(e)}}/>
+                            autoComplete="off" onChange={(e) => {handleChange(e)}}/>
                         </div>
                         <div className="w-full flex flex-row gap-9 pl-1 mb-2 items-center">
                             <label form="nomeFantasia" className=' text-xs' >Fantasia</label>
                                 <Input className="w-96 pl-1" size="sm" value={dataRepresentante?.nomeFantasiaRepresentante || ''} labelPlacement="outside" name={`nomeFantasia${type}`}
-                                placeholder=" " onChange={(e) => {handleChange(e)}}/>
+                                autoComplete="off" placeholder=" " onChange={(e) => {handleChange(e)}}/>
                         </div>
                         <div className="w-full flex flex-row gap-3 mb-2 items-center">
                             <div className="w-[60%] flex flex-row items-center pl-1 gap-7">
                                 <label className='text-xs'>CPF/CNPJ</label>
                                 <Input className="w-96 pl-0.5" size="sm" value={dataRepresentante?.cpfCnpjRepresentante || ''} labelPlacement="outside" name={`cpfCnpj${type}`}
-                                onChange={(e) => {handleChange(e)}}/>
+                                autoComplete="off" onChange={(e) => {handleChange(e)}}/>
                             </div>
                             <label className='text-xs pl-6'>IE/RG</label>
                             <Input className="w-[9rem]" size="sm" value={dataRepresentante?.ieRgRepresentante || ''} labelPlacement="outside" name={`ieRg${type}`}
-                            onChange={(e) => {handleChange(e)}}/>
+                            autoComplete="off" onChange={(e) => {handleChange(e)}}/>
                         </div>
                         <div className="w-full flex flex-row gap-2 pl-0.5 mb-2 items-center">
                             <label className='text-xs' >Orgão Emissor</label>
                             <Input className="w-30" size="sm" value={dataRepresentante?.orgaoEmissorRepresentante || ''} labelPlacement="outside" name={`orgaoEmissor${type}`}
-                            onChange={(e) => {handleChange(e)}}/>
+                            autoComplete="off" onChange={(e) => {handleChange(e)}}/>
                             <label className='text-xs' >UF</label>
                             <Input className="w-28" size="sm" value={dataRepresentante?.ufRgRepresentante || ''} labelPlacement="outside" name={`ufRg${type}`}
-                            onChange={(e) => {handleChange(e)}}/>
+                            autoComplete="off" onChange={(e) => {handleChange(e)}}/>
                             <label className='text-xs' >Data de Emissão</label>
                             <Input className="w-30" type="date" value={dataRepresentante?.dataEmissaoRepresentante || ''} size="sm" labelPlacement="outside" name={`dataEmissao${type}`}
-                            onChange={(e) => {handleChange(e)}}/>
+                            autoComplete="off" onChange={(e) => {handleChange(e)}}/>
                         </div>
                     </div>
                     </>
