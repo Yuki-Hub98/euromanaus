f3ce2be0dd3772d149eaeee24d2c2be90445020a
Yuki-Hub98 <wellyson.yago1@gmail.com>
2024-02-14 12:22:19 -0400

    Add: api de cadastro fornecedor

---
+++ src/pages/api/fornecedor/post.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const Post = async (nameRequest, data) => {
+    const response = await axios.post(`http://localhost:8080/${nameRequest}`, data)
+    .then( (response) => {
+        if (response) {
+            return response.data
+        }
+    })
+    .catch((error) => {
+        if (error.response) {
+            console.log("error", error.response)
+        }
+    })
+    return response
+}
+
+export default Post
