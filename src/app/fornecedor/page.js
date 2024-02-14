e8ba767cd4af815172255c653ef457ae2cdf1987
Yuki-Hub98 <wellyson.yago1@gmail.com>
2024-02-14 12:24:21 -0400

    Att: Adicionando propriedade post data para cadastrar fornecedor

--- src/app/fornecedor/page.js
+++ src/app/fornecedor/page.js
@@ -1,21 +1,49 @@
 "use client";
+import React, {useEffect, useState} from "react";
 import { Breadcrumbs, BreadcrumbItem } from "@nextui-org/react";
 import SuccessAlert from "../../components/SuccessAlert";
 import TopButtons from "@/components/TopButtons";
 import MiniSideBarButtons from "@/components/MiniSideBarButtons";
 import Table from "@/components/Table";
-import React, {useEffect, useState} from "react";
+import { PostFornecedor } from "../actions/fornecedor";
 
 export default function Fornecedor () {
-    const [option, setOption] = useState('fornecedor');
+    const option = "fornecedor"
     const [tableData, setTableData] = useState();
     const [status, setStatus] = useState();
     const [valueTable, setValueTable] = useState();
+    const [dataPost, setDataPost] = useState();
+    const [nameRequest, setNameRequest] = useState();
     
 
     const ValueTable = (value) => {
         setValueTable(value)
     }
+
+    const PostData = (nameRequest, data) => {
+        console.log(nameRequest, data)
+        setDataPost(data)
+        setNameRequest(nameRequest)
+    }
+
+    useEffect(() => {
+        if(dataPost) {
+            Resgister(nameRequest, dataPost)
+        }
+    })
+    const Clear = () => {
+        setDataPost(null)
+        setNameRequest(null)
+    }
+
+    const Resgister = async (nameRequest, data) =>{
+        if (data) {
+            const statuData = await PostFornecedor(nameRequest, data);
+            setStatus(statuData);
+            return Clear();
+        }
+    }
+
 
     const CloseStatus = () => {
         return setStatus(null);
@@ -31,11 +59,11 @@ export default function Fornecedor () {
                         <BreadcrumbItem>Fornecedor</BreadcrumbItem>
                     </Breadcrumbs>
             </div>
-                { status?.descricao ? ( <> <SuccessAlert CloseStatus={CloseStatus} message="Cadastro efetuado com"/> </> ): (null) }
+                { status?.razaoSocialFornecedor ? ( <> <SuccessAlert CloseStatus={CloseStatus} message="Cadastro efetuado com"/> </> ): (null) }
                 { status?.del ? ( <> <SuccessAlert CloseStatus={CloseStatus} message="Deletado com"/> </> ): (null) }
             <TopButtons title={option} />
             <div className='flex h-4/5 overflow-y-auto mt-1.5 w-full flex-row'>
-                <MiniSideBarButtons />
+                <MiniSideBarButtons name={option} PostData={PostData}/>
                 <div className='w-full flex overflow-y-auto h-50 flex-col rounded'>
                     <div className='w-full top-0 overflow-y-auto h-full bg-[#EDEDED]'>
                         <Table data={tableData} name={option} ValueTable={ValueTable} />
