e14298bdb664b0b360bd9e81f4d265ba6b849896
Yuki-Hub98 <wellyson.yago1@gmail.com>
2024-02-16 15:03:08 -0400

    Att: Agora o modulo forncedor pesquisa e cadastra, reescrevi algumas variaveis e funções para ficar mais dinamico

--- src/app/fornecedor/page.js
+++ src/app/fornecedor/page.js
@@ -1,19 +1,17 @@
 "use client";
-import React, {useEffect, useState} from "react";
+import React, {useState} from "react";
 import { Breadcrumbs, BreadcrumbItem } from "@nextui-org/react";
 import SuccessAlert from "../../components/SuccessAlert";
 import TopButtons from "@/components/TopButtons";
 import MiniSideBarButtons from "@/components/MiniSideBarButtons";
-import Table from "@/components/Table";
-import { PostFornecedor } from "../actions/fornecedor";
+import TableRender from "@/components/TableRender";
+import { PostFornecedor, GetFornecedor } from "../actions/fornecedor";
 
 export default function Fornecedor () {
     const option = "fornecedor"
-    const [tableData, setTableData] = useState();
+    const [tableData, setTableData] = useState([{}]);
     const [status, setStatus] = useState();
     const [valueTable, setValueTable] = useState();
-    const [dataPost, setDataPost] = useState();
-    const [nameRequest, setNameRequest] = useState();
     
 
     const ValueTable = (value) => {
@@ -21,29 +19,36 @@ export default function Fornecedor () {
     }
 
     const PostData = (nameRequest, data) => {
-        console.log(nameRequest, data)
-        setDataPost(data)
-        setNameRequest(nameRequest)
+        if (data) {
+            Resgister(nameRequest, data)
+        }
     }
 
-    useEffect(() => {
-        if(dataPost) {
-            Resgister(nameRequest, dataPost)
+    const GetData = (nameRequest, data) => {
+        if (data) {
+            Search(nameRequest, data);
+        }else{
+            Search(nameRequest)
         }
-    })
-    const Clear = () => {
-        setDataPost(null)
-        setNameRequest(null)
+        
     }
 
     const Resgister = async (nameRequest, data) =>{
         if (data) {
-            const statuData = await PostFornecedor(nameRequest, data);
-            setStatus(statuData);
-            return Clear();
+            const statusData = await PostFornecedor(nameRequest, data);
+            setStatus(statusData);
         }
     }
 
+    const Search = async (nameRequest, dataGet) =>{
+        if (dataGet) {
+            const data = await GetFornecedor(nameRequest, dataGet);
+            setTableData(data)
+        }else{
+            const data = await GetFornecedor(nameRequest)
+            setTableData(data)
+        }
+    } 
 
     const CloseStatus = () => {
         return setStatus(null);
@@ -59,15 +64,13 @@ export default function Fornecedor () {
                         <BreadcrumbItem>Fornecedor</BreadcrumbItem>
                     </Breadcrumbs>
             </div>
-                { status?.razaoSocialFornecedor ? ( <> <SuccessAlert CloseStatus={CloseStatus} message="Cadastro efetuado com"/> </> ): (null) }
-                { status?.del ? ( <> <SuccessAlert CloseStatus={CloseStatus} message="Deletado com"/> </> ): (null) }
-            <TopButtons title={option} />
+                { status?.razaoSocialFornecedor ? ( <> <SuccessAlert CloseStatus={CloseStatus} message="Cadastro efetuado com Sucesso !"/> </> ): (null) }
+                { status?.del ? ( <> <SuccessAlert CloseStatus={CloseStatus} message="Deletado com com Sucesso !"/> </> ): (null) }
+            <TopButtons title={option} option={option} GetData={GetData} />
             <div className='flex h-4/5 overflow-y-auto mt-1.5 w-full flex-row'>
                 <MiniSideBarButtons name={option} PostData={PostData}/>
                 <div className='w-full flex overflow-y-auto h-50 flex-col rounded'>
-                    <div className='w-full top-0 overflow-y-auto h-full bg-[#EDEDED]'>
-                        <Table data={tableData} name={option} ValueTable={ValueTable} />
-                    </div>
+                    <TableRender data={tableData} name={option} ValueTable={ValueTable} />
                     <div className='w-full h-10 bg-[#CFCFCF]'>
                         <span className='text-black'>
                             teste teste teste teste
