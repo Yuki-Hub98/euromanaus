a4d3a634329595341173cbab0622c2180b12d3ce
Yuki-Hub98 <wellyson.yago1@gmail.com>
2024-02-16 14:59:18 -0400

    Att: Aproveitei que reescrevi a estrutura de arvore de produto, e reescrevi a requisição get também da arvore de produto

--- src/pages/api/arvore-produto/get.js
+++ src/pages/api/arvore-produto/get.js
@@ -2,16 +2,25 @@ import axios from "axios";
 
 const Get = async (request, data) =>{
     if (data){
-        const response = await axios.get(`http://localhost:8080/arvore-produto/${request}/search`,{params: data})
-        .then((response) => response.data )
-        .catch((error) => error.response)
-        return response
-    }else{
-        const response = await axios.get(`http://localhost:8080/arvore-produto/${request}`)
-        .then((response) => response.data )
-        .catch((error) => error.response)
+        const response = await axios.get(`http://localhost:8080/arvore-produto/${request}/search?descricao=${data?.descricao}`)
+        .then((response) => {
+            if (response) {
+                return response.data
+            }
+        })
+        .catch((error) => {
+            if (error) {
+                return error.response
+            }
+        })
         return response
     }
+        
+    const response = await axios.get(`http://localhost:8080/arvore-produto/${request}`)
+    .then((response) => response.data )
+    .catch((error) => error.response)
+    return response
+    
 }
 
 export default Get
