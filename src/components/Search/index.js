8f3296d9a285492afc800c9b527557c3d49369da
Yuki-Hub98 <wellyson.yago1@gmail.com>
2024-02-16 14:50:12 -0400

    Att: Redesenhando Search para virar um componente comum de pesquisa com parametro, agora tenho a liberdade de reutilizar em varias paginas

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -2,94 +2,118 @@
 import React , {useEffect, useState} from "react";
 import {Button,  Input} from "@nextui-org/react";
 import RegexToSave from "@/functions/regexToSave";
+import { usePathname } from "next/navigation";
 
-const SearchArvore = (props) => {
+const Search = (props) => {
+    const router = usePathname()
     const [dataToGet, setDataToGet] = useState();
     const [dataSearchDesc, setDataSearchDesc] = useState();
     const {ReceiveGet} = props
-    /*
-    const [dataSearchOut, setDataSearchOut] = useState();
-    const section = (name) => {
-        switch (name) {
-            case "Linha":
-                return "Departamento"
-            case "Familia":
-                return "Linha"
-            case "Grupo":
-                return "Familia"
-            default:
-                break;
-        }
-    }
-    
-    const FormateToGet = (opcao) => {
-        switch (opcao) {
-            case 'Departamento':
-                setDataToGet({'descricao': dataSearchDesc})
-                break;
-            case 'Linha':
-                setDataToGet({'departamento': dataSearchOut, 'descricao': dataSearchDesc})
-                break;
-            case 'Familia':
-                setDataToGet({'linha': dataSearchOut, 'descricao': dataSearchDesc})
-                break;
-            case 'Grupo':
-                setDataToGet({'familia': dataSearchOut, 'descricao': dataSearchDesc})
-                break;
-            case 'Cor':
-                setDataToGet({'descricao': dataSearchDesc})
-                break;
-            case 'Especificação':
-                setDataToGet({'descricao': dataSearchDesc})
-                break;
-            default:
-                break;
-        }
-    }
-*/
+    const { data } = props
     const Clear = () => {
         setDataToGet(null)
         setDataSearchDesc(null)
     }
 
+    const handleChange = (e) =>{
+        const {value, ariaLabel} = e.target
+        switch (ariaLabel) {
+            case "Razão Social":
+                setDataSearchDesc(dataSearch => ({
+                    ...dataSearch,
+                    razaoSocialFornecedor: value
+                }))
+                break;
+            case "CPF/CNPJ":
+                setDataSearchDesc(dataSearch => ({
+                    ...dataSearch,
+                    cpfCnpjFornecedor: value
+                }))
+                break;
+            case "Nome Fantasia":
+                setDataSearchDesc(dataSearch => ({
+                        ...dataSearch,
+                        nomeFantasiaFornecedor: value
+                }))
+                break;
+            case "Descrição":
+                setDataSearchDesc(dataSearch => ({
+                    ...dataSearch,
+                    descricao: RegexToSave(value)
+            }))
+            default:
+                break;
+        }
+    }
+
+    console.log(dataSearchDesc)
     const Click = () => {
         if (dataSearchDesc) {
-            return setDataToGet({'descricao': dataSearchDesc})
+            return setDataToGet(dataSearchDesc)
         }else{
-            return ReceiveGet(props?.data?.option);
+            return ReceiveGet(data?.option);
         }
     }
 
     useEffect(() => {
         if (dataToGet) {
-            ReceiveGet(props?.data?.option, dataToGet);
+            ReceiveGet(data?.option, dataToGet);
             Clear();
         }
     })
+    
+    const optionPage = (page) => {
+        switch(page){
+            case "/arvore-de-produtos":
+                return(
+                    <>
+                    <div className='h-12 flex flex-row justify-items-center items-center'>
+                        <Input className='w-64' labelPlacement='outside-left' placeholder=" " 
+                        onChange={(e) => handleChange(e)} color="primary" label="Descrição"/>
+                        <Button color="primary" size='sm' variant="ghost" onClick={() =>  Click()} >
+                            Pesquisar
+                        </Button>
+                    </div>
+                    </>
+                )
+            case "/fornecedor":
+                return(
+                    <>
+                    <div className='flex flex-row justify-center items-center gap-2'>
+                        <Input  className='w-64'
+                        label="Razão Social"
+                        labelPlacement={"outside"}
+                        placeholder=" "
+                        size="sm"
+                        onChange={(e) => handleChange(e)} color="primary"/>
+                        <Input  className='w-64'
+                        label="CPF/CNPJ"
+                        labelPlacement={"outside"}
+                        placeholder=" "
+                        size="sm"
+                        onChange={(e) => handleChange(e)} color="primary"/>
+                        <Input  className='w-64'
+                        label="Nome Fantasia"
+                        labelPlacement={"outside"}
+                        placeholder=" "
+                        size="sm"
+                        onChange={(e) => handleChange(e)} color="primary"/>
+                        <div className="pt-6">
+                        <Button color="primary" size='sm' variant="ghost"  onClick={() =>  Click()}>
+                            Pesquisar
+                        </Button>
+                        </div>
+                    </div>
+                    </>
+                )
+        }
+    }
 
     return(
-        <form>
-        <div className='h-12 flex flex-row justify-items-center items-center'>
-                <Input className='w-80' labelPlacement={"outside-left"} onChange={(e) => setDataSearchDesc(RegexToSave(e.target.value))} color="primary" label="Descrição"/>
-                {/*{data?.data?.type === 1 ? 
-                <>
-                <div className='pb-6'>
-                    <Input className='w-80'  labelPlacement={'outside'} onChange={(e) => setDataSearchDesc(RegexToSave(e.target.value))} color="primary" label="Descrição"/>
-                </div>
-                </>
-                :
-                <>
-                    <Input className='w-80'  labelPlacement={'outside'} onChange={(e) => setDataSearchOut(RegexToSave(e.target.value))} color="primary" label={section(data?.data?.name)}/>
-                    <Input className='w-80'  labelPlacement={'outside'} onChange={(e) => setDataSearchDesc(RegexToSave(e.target.value))} color="primary" label="Descrição"/>
-                </>
-                } */}
-                
-            <Button color="primary" size="sm" variant="ghost" onClick={() =>  Click()} >
-                Pesquisar
-            </Button>
-        </div>
-        </form>
+        <>
+        {optionPage(router)}
+        </>
     )
 }
 
-export default SearchArvore
+export default Search
