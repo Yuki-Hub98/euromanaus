e4d972fad984a53ad589ec3d11f3e5b74d726ff0
Yuki-Hub98 <wellyson.yago1@gmail.com>
2024-02-20 10:16:43 -0400

    Fix: A antiga codicional fazia um tipo de requisição que é quando data tinha mais de um parametro

--- src/pages/api/fornecedor/cep.js
+++ src/pages/api/fornecedor/cep.js
@@ -3,34 +3,33 @@ import axios from "axios";
 const Cep = async (data) => {
 
 		if (data?.endereco && data?.cidade && data?.uf) {
-				const response = await axios.get(`https://viacep.com.br/ws/${data?.uf}/${data?.cidade}/${data?.endereco}/json/`)
+				const response = await axios.get(`https://viacep.com.br/ws/${data?.uf}/${data?.cidade}/${data?.endereco.replace(/\s/g, '+')}/json/`)
 				.then((response) => {
 						if (response) {
-								return response.data
-								}
+							return response.data
+						}
 				})
 				.catch((error) => {
 						if (error) {
-								console.log("error: ", error)
+							console.log("error: ", error)
 						}
 				})
-
 				return response
-		}
+		}else{
 
-		const response = await axios.get(`https://viacep.com.br/ws/${data}/json/`)
-		.then((response) => {
-				if (response) {
+			const response = await axios.get(`https://viacep.com.br/ws/${data}/json/`)
+			.then((response) => {
+					if (response) {
 						return response.data
-				}
-		})
-		.catch((error) => {
-				if (error) {
+					}
+			})
+			.catch((error) => {
+					if (error) {
 						console.log("error: ", error)
-				}
-		})
-
-		return response 
+					}
+			})
+			return response 
+		}
 }
 
 export default Cep
