e2ac9a18f5f4eefc04c69d698c39a4c1dcdc2357
Yuki-Hub98 <wellyson.yago1@gmail.com>
2024-02-28 09:36:16 -0400

    Feat(action e api/ Arvore Produto): Adicionada nova opção de requisição por flag, para pesquisar por departamento

--- src/pages/api/arvore-produto/get.js
+++ src/pages/api/arvore-produto/get.js
@@ -1,6 +1,21 @@
 import axios from "axios";
 
-const Get = async (request, data) =>{
+const Get = async (request, data, flag) =>{
+		if (flag){
+			const response = await axios.get(`http://localhost:8080/arvore-produto/${request}/search/${data}`)
+				.then((response) => {
+						if (response) {
+								return response.data
+						}
+				})
+				.catch((error) => {
+						if (error) {
+								return error.response
+						}
+				})
+				return response
+		}
+
 		if (data){
 				const response = await axios.get(`http://localhost:8080/arvore-produto/${request}/search?descricao=${data?.descricao}`)
 				.then((response) => {
