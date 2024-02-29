939315500a62f0b1027626a48aea8d30327cf16d
Yuki-Hub98 <wellyson.yago1@gmail.com>
2024-02-29 08:20:47 -0400

    Feat(data): foi criada uma pasta data, para ficar fixo alguns dados na aplicação

---
+++ src/data/data.js
@@ -0,0 +1,27 @@
+export const unidadeMedida = [ 
+  {medida:"M"}, 
+  {medida:"UND"}, 
+  {medida:"CX"}, 
+  {medida:"MIL"}, 
+  {medida:"PC"}, 
+  {medida:"PAR"}, 
+  {medida:"TON"}, 
+  {medida:"KG"}, 
+  {medida:"CJ"}, 
+  {medida:"M²"}, 
+  {medida:"RL"}, 
+  {medida:"M³"}, 
+  {medida:"CH"}, 
+  {medida:"CT"}, 
+  {medida:"GL"}, 
+  {medida:"CM"},
+  {medida:"L"},
+  {medida:"ML"}
+]
+
+export const tipoProduto = [ 
+  {status:"ATIVO IMOBILIARIO"},
+  {status:"ITEM DE VENDA"},
+  {status:"MATÉRIA PRIMA"},
+  {status:"USO E CONSUMO"}
+]
