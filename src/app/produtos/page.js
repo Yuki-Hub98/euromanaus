56066c85798a87a836785b2607313981b1c16ef0
Yuki-Hub98 <wellyson.yago1@gmail.com>
2024-03-04 10:50:10 -0400

    Feat(Produto): Agora o modulo produto está cadastrando, ja api já está funcionando

--- src/app/produtos/page.js
+++ src/app/produtos/page.js
@@ -4,13 +4,29 @@ import React , {useState} from "react";
 import TopButtons from "@/components/TopButtons";
 import TableRender from "@/components/TableRender";
 import MiniSideBarButtons from "@/components/MiniSideBarButtons";
+import { PostProduto } from "../actions/produto";
+import SuccessAlert from "@/components/SuccessAlert";
 
 export default function Produtos () {
 	const option = "produtos"
 	const [tableData, setTableData] = useState([{}]);
 	const [status, setStatus] = useState();
 	const [valueTable, setValueTable] = useState();
+	
+	const PostData = (nameRequest, data) => {
+		if (data) {
+			Register(nameRequest, data)
+		}
+	}
 
+	const Register = async (nameRequest, data) => {
+		const status = await PostProduto(nameRequest, data)
+		setStatus(status)
+	}
+
+	const CloseStatus = () =>{
+		setStatus(null)
+	} 
 
 	return (
 		<>
@@ -22,9 +38,10 @@ export default function Produtos () {
 						<BreadcrumbItem>Produtos</BreadcrumbItem>
 					</Breadcrumbs>
 			</div>
+			{status?.descricaoProduto ? (<> <SuccessAlert CloseStatus={CloseStatus} message="Cadastro efetuado com Sucesso !"/> </> ): (null)}
 			<TopButtons title={option} option={option}/>
 			<div className='flex h-4/5 overflow-y-auto mt-1.5 w-full flex-row'>
-				<MiniSideBarButtons name={option} />
+				<MiniSideBarButtons name={option} PostData={PostData} />
 				<div className='w-full flex overflow-y-auto h-50 flex-col rounded'>
 					<TableRender data={tableData} name={option} />
 					<div className='w-full h-10 bg-[#CFCFCF]'>
