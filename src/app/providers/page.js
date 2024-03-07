c11b79192a13a9531e567ab1168c58e4b2e34dc9
Yuki-Hub98 <wellyson.yago1@gmail.com>
2024-03-07 14:57:05 -0400

    Feat(Providers): retirando recoil

--- src/app/providers/page.js
+++ src/app/providers/page.js
@@ -1,13 +1,11 @@
 'use client';
 import {NextUIProvider} from '@nextui-org/react'
-import { RecoilRoot } from 'recoil'
 
 export default function Provider({children}){
 	return (
-		<RecoilRoot>
-			<NextUIProvider className='h-full'>
-				{children}
-			</NextUIProvider>
-		</RecoilRoot>
+		<NextUIProvider className='h-full'>
+			{children}
+		</NextUIProvider>
+
 	)
 }
