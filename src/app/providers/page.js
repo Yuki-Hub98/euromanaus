5c768327ec02989e234f9448ed0cb9c904f88f08
Yuki-Hub98 <wellyson.yago1@gmail.com>
2024-03-18 14:28:32 -0400

    feat(Styles): Ajuste na configuração de tailwind, e no css, NextUiProvider agora usa router para passar estilos por parametro

--- src/app/providers/page.js
+++ src/app/providers/page.js
@@ -1,9 +1,11 @@
 'use client';
 import {NextUIProvider} from '@nextui-org/react'
+import {useRouter} from 'next/navigation'
 
 export default function Provider({children}){
+	const router = useRouter();
 	return (
-		<NextUIProvider className='h-full'>
+		<NextUIProvider className='h-full' navigate={router.push}>
 			{children}
 		</NextUIProvider>
 
