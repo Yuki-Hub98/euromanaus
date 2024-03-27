84d21cd55fb5b795143039973781689506128264
Yuki-Hub98 <wellyson.yago1@gmail.com>
2024-03-27 17:26:21 -0400

    Feat(Reducer):Nova implementação de reducer para verificar os dados que vão para a tabela, manipula post, get, put, delete, conforme vou fazendo requisição a tabela é alterada dinamicamente

--- src/hooks/services/useDeleteData.js
+++ src/hooks/services/useDeleteData.js
@@ -1,23 +1,39 @@
 "use client";
-import SuccessAlert from "@/components/ui/SuccessAlert";
-import Warning from "@/components/ui/Warning";
+import SuccessAlert from "@/components/ui/successAlert";
+import Warning from "@/components/ui/warning";
 import {useState} from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 const useDeleteData = (deleteFunction) => {
-  const [resultDelte, setResultDelete] = useState();
-
+  const resultDelte = useSelector(state => state.data)
+  const [status, setStatus] = useState()
+  const dispatch = useDispatch();
   const CloseStatus = () => {
-		return setResultDelete(null);
+		return setStatus(null);
 	}
 
   const DeleteData = async (nameRequest, data) => {
-		const statusDel = await deleteFunction(nameRequest,data)
-		setResultDelete(statusDel)
+    try{
+      const statusDel = await deleteFunction(nameRequest,data)
+      console.log(statusDel)
+      dispatch({type:'DELETE_DATA', payload: statusDel})
+      setStatus(statusDel)
+    }catch{
+      const statusError = {
+        status: 404,
+        error: "Erro ao deletar dados",
+        message: "Por favor contacte os administradores"
+      }
+      setStatus(statusError)
+    }
+
+    
+    
 	}
 
-  let statusDelete =  resultDelte?.del ? ( <> <SuccessAlert CloseStatus={CloseStatus}  message="Deletado com com Sucesso !"/> </> ): (null)
+  let statusDelete =  status?.del ? ( <> <SuccessAlert CloseStatus={CloseStatus}  message="Deletado com com Sucesso !"/> </> ): (null)
 
-  let warningDelete = resultDelte?.error ? ( <> <Warning status={resultDelte} CloseStatus={CloseStatus} /> </>) : (null) 
+  let warningDelete = status?.error ? ( <> <Warning status={status} CloseStatus={CloseStatus} /> </>) : (null) 
 
   return {resultDelte, statusDelete, warningDelete, DeleteData}
 
